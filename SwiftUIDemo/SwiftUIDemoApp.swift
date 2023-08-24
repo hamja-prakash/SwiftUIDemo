532af21cd04bb9d1e736ef458907aaf7fa0be8c0
Alex Samy <alex.samy@geokeyaccess.com>
2023-08-24 09:33:41 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Admin on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
